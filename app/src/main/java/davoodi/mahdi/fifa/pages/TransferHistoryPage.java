ce571270c0ba7563077b987dd61090e5bb2f67ec
mahdidavoodi <mahdidavoodi@gmail.com>
2021-05-25 15:55:17 +0300

    Finished TransferHistoryAdapter.java and TransferHistoryPage.java (Not Tested)

--- app/src/main/java/davoodi/mahdi/fifa/pages/TransferHistoryPage.java
+++ app/src/main/java/davoodi/mahdi/fifa/pages/TransferHistoryPage.java
@@ -1,16 +1,25 @@
 package davoodi.mahdi.fifa.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import davoodi.mahdi.fifa.R;
+import davoodi.mahdi.fifa.adapters.TransferHistoryAdapter;
 
 public class TransferHistoryPage extends AppCompatActivity {
+    RecyclerView transfer_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transfer_history_page);
+
+        transfer_list = findViewById(R.id.th_list);
+        transfer_list.setHasFixedSize(true);
+        transfer_list.setLayoutManager(new LinearLayoutManager(this));
+        transfer_list.setAdapter(new TransferHistoryAdapter(this));
     }
 }
