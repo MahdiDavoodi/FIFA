750843b9350b7906c81ed0e7e90a7983e0c81fa6
mahdidavoodi <mahdidavoodi@gmail.com>
2021-05-28 03:46:59 +0300

    some changes before release

--- app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
+++ app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
@@ -2,11 +2,16 @@ package davoodi.mahdi.oufa.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import davoodi.mahdi.oufa.R;
 
 public class InfoPage extends AppCompatActivity {
+
+    public static final String REPOSITORY = "https://github.com/MahdiDavoodi/OUFA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,10 @@ public class InfoPage extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.activity_slide_from_left, R.anim.activity_slide_to_right);
+    }
+
+    // Github OnClick.
+    public void openGithub(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(REPOSITORY)));
     }
 }
