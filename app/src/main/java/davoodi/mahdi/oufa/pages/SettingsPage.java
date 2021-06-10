c34351cc542364ff86e25c68afe25a40685c84cc
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-10 19:08:37 +0300

    Start working on SettingsPage

---
+++ app/src/main/java/davoodi/mahdi/oufa/pages/SettingsPage.java
@@ -0,0 +1,16 @@
+package davoodi.mahdi.oufa.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import davoodi.mahdi.oufa.R;
+
+public class SettingsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_page);
+    }
+}
