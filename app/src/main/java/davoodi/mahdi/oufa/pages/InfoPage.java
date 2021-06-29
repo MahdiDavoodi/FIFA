afa85a938b1f7e31f273df938f2d4391f70ad51f
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-29 19:22:51 +0300

    Replaced myket vote page to myket application page for now!

--- app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
+++ app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
@@ -3,14 +3,9 @@ package davoodi.mahdi.oufa.pages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import java.util.List;
 
 import davoodi.mahdi.oufa.R;
 
@@ -18,7 +13,7 @@ public class InfoPage extends AppCompatActivity {
 
     public static final String REPOSITORY = "https://github.com/MahdiDavoodi/OUFA";
 
-    public static final String MYKET_VOTE = "myket://comment?id=davoodi.mahdi.oufa";
+    public static final String MYKET_APP = "https://myket.ir/app/davoodi.mahdi.oufa";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +34,6 @@ public class InfoPage extends AppCompatActivity {
 
     // Myket OnClick.
     public void openMyket(View view) {
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities( mainIntent, 0);
-
-        for (ResolveInfo packageInfo : pkgAppsList) {
-            Log.d("ZZZ", "Installed package :" + packageInfo.resolvePackageName);
-        }
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(MYKET_APP)));
     }
 }
