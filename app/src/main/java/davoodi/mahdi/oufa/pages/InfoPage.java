0caa3a82e0868dfca44774846969054ebab0a646
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-29 14:45:51 +0300

    Changed the icon, implemented Myket.ir vote intent and etc.

--- app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
+++ app/src/main/java/davoodi/mahdi/oufa/pages/InfoPage.java
@@ -13,6 +13,8 @@ public class InfoPage extends AppCompatActivity {
 
     public static final String REPOSITORY = "https://github.com/MahdiDavoodi/OUFA";
 
+    public static final String MYKET_VOTE = "myket://comment?id=davoodi.mahdi.oufa";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +30,10 @@ public class InfoPage extends AppCompatActivity {
     // Github OnClick.
     public void openGithub(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(REPOSITORY)));
+    }
+
+    // Myket OnClick.
+    public void openMyket(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(MYKET_VOTE)));
     }
 }
